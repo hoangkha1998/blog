4a08f1fcdd982d65247b746ab65d3067e05589fa
nguyen_kha <nguyen_kha@kozo-japan.com>
2021-04-06 13:51:16 +0700

    add template

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,21 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
+});
+Route::get('/blog', function () {
+    return view('blog');
+});
+Route::get('/about', function () {
+    return view('about');
+});
+Route::get('/contact', function () {
+    return view('contact');
+});
+Route::get('/team', function () {
+    return view('team');
+});
+
+Route::prefix('admin')->group(function () {
+    
 });
