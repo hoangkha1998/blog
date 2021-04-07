ad27b753271342bbaeaf229111c3e0825b5b4346
nguyen_kha <nguyen_kha@kozo-japan.com>
2021-04-07 11:45:36 +0700

    delete code not use

--- resources/views/admin/common/slider.blade.php
+++ resources/views/admin/common/slider.blade.php
@@ -23,6 +23,19 @@
                           </ul>
                       </div>
                   </li>
+                  <li class="nav-item">
+                    <a class="nav-link" href="#" data-toggle="collapse" aria-expanded="false" data-target="#submenu-2" aria-controls="submenu-2"><i class="fa fa-fw fa-rocket"></i>Users</a>
+                    <div id="submenu-2" class="collapse submenu" style="">
+                        <ul class="nav flex-column">                            
+                            <li class="nav-item">
+                                <a class="nav-link" href="pages/general.html">List</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="pages/carousel.html">Add</a>
+                            </li>                            
+                        </ul>
+                    </div>
+                </li>
                   {{-- <li class="nav-item">
                       <a class="nav-link" href="#" data-toggle="collapse" aria-expanded="false" data-target="#submenu-2" aria-controls="submenu-2"><i class="fa fa-fw fa-rocket"></i>UI Elements</a>
                       <div id="submenu-2" class="collapse submenu" style="">
