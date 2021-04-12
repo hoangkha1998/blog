6be0e1e1ed460ef5ee714b0cf9241c8352f1f9e2
nguyen_kha <nguyen_kha@kozo-japan.com>
2021-04-12 08:27:32 +0700

    add .....

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -17,7 +17,8 @@ class Post extends Model
         'slug',
         'content',
         'isBanner',
-        'isDisplay'
+        'isDisplay',
+        'images'
     ];
 
     public function author()
